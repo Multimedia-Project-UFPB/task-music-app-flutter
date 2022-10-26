a04972ccac5e83b1b678cb15d833ac5003840202
LeandriSimo <leo.bt.12@hotmail.com>
2022-10-26 20:03:07 -0300

    Creating input_button.dart

---
+++ lib/app/widgets/input_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+class InputButton extends StatelessWidget {
+  final IconData icon;
+  final void Function()? function;
+
+  const InputButton({
+    Key? key,
+    required this.icon,
+    this.function,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (_) => IconButton(
+        onPressed: function,
+        icon: Icon(icon),
+      ),
+    );
+  }
+}
