7b6108a13df02276c695f62658e9adea5b3959b6
LeandriSimo <leo.bt.12@hotmail.com>
2022-10-26 21:01:32 -0300

    Creating nav_bar.dart

---
+++ lib/app/widgets/nav_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Navbar extends StatelessWidget {
+  const Navbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      unselectedItemColor: Colors.white,
+      selectedItemColor: Colors.white,
+      showUnselectedLabels: false,
+      showSelectedLabels: false,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_outline),
+          label: 'Favorites',
+        ),
+      ],
+    );
+  }
+}
